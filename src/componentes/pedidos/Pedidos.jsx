8f9fa0417567c4ac7fef92e5d8d036d399cc02a9
Fabeto19 <fabriciopolanco18@gmail.com>
2023-06-10 18:12:37 -0600

    Junio 10

--- src/componentes/pedidos/Pedidos.jsx
+++ src/componentes/pedidos/Pedidos.jsx
@@ -1,14 +1,63 @@
-
-import './pedidos.css'
+import React from 'react';
+import './pedidos.css';
+import imgprueba from './mona1.png';
 
 const Pedidos = () => {
+  return (
+    <div className='contenedor'>
+      <h2 className='titulo'>Tus Pedidos</h2>
+      <div className='cont-imagenes'>
+        <a href='#' className='enlace-imagen'>
+          <div className='imagen padre'>
+            <img className='imagen-producto' src={imgprueba} alt='' />
+            <h3 className='subtitulo'>Nombre Producto</h3>
+          </div>
+        </a>
+        <a href='#' className='enlace-imagen'>
+          <div className='imagen padre'>
+            <img className='imagen-producto' src={imgprueba} alt='' />
+            <h3 className='subtitulo'>Nombre Producto</h3>
+          </div>
+        </a>
+        <a href='#' className='enlace-imagen'>
+          <div className='imagen padre'>
+            <img className='imagen-producto' src={imgprueba} alt='' />
+            <h3 className='subtitulo'>Nombre Producto</h3>
+          </div>
+        </a>
+        <a href='#' className='enlace-imagen'>
+          <div className='imagen padre'>
+            <img className='imagen-producto' src={imgprueba} alt='' />
+            <h3 className='subtitulo'>Nombre Producto</h3>
+          </div>
+        </a>
+        <a href='#' className='enlace-imagen'>
+          <div className='imagen padre'>
+            <img className='imagen-producto' src={imgprueba} alt='' />
+            <h3 className='subtitulo'>Nombre Producto</h3>
+          </div>
+        </a>
+        <a href='#' className='enlace-imagen'>
+          <div className='imagen padre'>
+            <img className='imagen-producto' src={imgprueba} alt='' />
+            <h3 className='subtitulo'>Nombre Producto</h3>
+          </div>
+        </a>
+        <a href='#' className='enlace-imagen'>
+          <div className='imagen padre'>
+            <img className='imagen-producto' src={imgprueba} alt='' />
+            <h3 className='subtitulo'>Nombre Producto</h3>
+          </div>
+        </a>
+        <a href='#' className='enlace-imagen'>
+          <div className='imagen padre'>
+            <img className='imagen-producto' src={imgprueba} alt='' />
+            <h3 className='subtitulo'>Nombre Producto</h3>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
 
-
-    return(
-        <div className='ped'>
-            <h2>Este el componente pedidos</h2>
-        </div>
-    );
-}
-
-export default Pedidos
+export default Pedidos;
