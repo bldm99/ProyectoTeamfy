eef77b4e81590bdbb1d1d1225d2f1e7b98051bc4
Fabeto19 <fabriciopolanco18@gmail.com>
2023-06-25 12:31:36 -0600

    Avance 25/06

--- src/componentes/pedidos/Pedidos.jsx
+++ src/componentes/pedidos/Pedidos.jsx
@@ -1,11 +1,11 @@
-
-import './pedidos.css'
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './pedidos.css';
 
 const Pedidos = () => {
   const [images, setImages] = useState([]);
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetchImages();
@@ -13,14 +13,13 @@ const Pedidos = () => {
 
   const fetchImages = async () => {
     try {
-      const response = await axios.get(
-        'https://teamapi.bladimirchipana.repl.co/pruebas'
-      );
+      const response = await axios.get('https://teamapi-1.bladimirchipana.repl.co/userpr');
       setImages(response.data);
     } catch (error) {
       console.error('Error fetching images:', error);
     }
   };
+  
 
   const showSlide = (index) => {
     if (index < 0) {
@@ -39,46 +38,77 @@ const Pedidos = () => {
     showSlide(index - 1);
   };
 
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredImages = images.filter((image) =>
+    image.nombre.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="carousel">
-      <br></br>
-      <div
-        className="carousel-inner"
-        style={{
-          width: `${images.length * 100}%`,
-          transform: `translateX(-${currentSlide * (100 / images.length)}%)`,
-        }}
-      >
-        {images.map((image, index) => (
-          <a key={index} href="/ruta-de-destino" className="slide"> {/* Reemplaza "/ruta-de-destino" con la ruta adecuada */}
-            <div className="image-container">
+    <div>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          className="search-input"
+        />
+        <button className="search-button">Search</button>
+      </div>
+      <div className="carousel">
+        <div
+          className="carousel-inner"
+          style={{
+            width: `${filteredImages.length * 100}%`,
+            transform: `translateX(-${currentSlide * (100 / filteredImages.length)}%)`,
+          }}
+        >
+          {filteredImages.map((image, index) => (
+            <a key={index} href="/ruta-de-destino" className="slide">
+              <div className="image-container">
+                <img src={image.imagen} alt={`Imagen ${index + 1}`} />
+              </div>
+              <div className="image-info">
+                <div className="image-name">{image.nombre}</div>
+                <div className="image-price">{`Price: $${image.precio}`}</div>
+              </div>
+            </a>
+          ))}
+        </div>
+        <a className="prev" onClick={() => changeSlide(-1)}>
+          &#10094;
+        </a>
+        <a className="next" onClick={() => changeSlide(1)}>
+          &#10095;
+        </a>
+        <div className="indicators">
+          {filteredImages.map((_, index) => (
+            <button
+              key={index}
+              className={`indicator ${currentSlide === index ? 'active' : ''}`}
+              onClick={() => goToSlide(index + 1)}
+            ></button>
+          ))}
+        </div>
+      </div>
+      <div className="product-grid">
+        {filteredImages.map((image, index) => (
+          <a key={index} href="/ruta-de-destino" className="product-item">
+            <div className="product-image">
               <img src={image.imagen} alt={`Imagen ${index + 1}`} />
             </div>
-            <div className="image-info">
-              <div className="image-name">{image.nombre}</div>
-              <div className="image-price">{`Price: $${image.precio}`}</div>
+            <div className="product-info">
+              <div className="product-name">{image.nombre}</div>
+              <div className="product-price">{`Price: $${image.precio}`}</div>
             </div>
           </a>
-        ))}
-      </div>
-      <a className="prev" onClick={() => changeSlide(-1)}>
-        &#10094;
-      </a>
-      <a className="next" onClick={() => changeSlide(1)}>
-        &#10095;
-      </a>
-      <div className="indicators">
-        {images.map((_, index) => (
-          <button
-            key={index}
-            className={`indicator ${currentSlide === index ? 'active' : ''}`}
-            onClick={() => goToSlide(index + 1)}
-          ></button>
         ))}
       </div>
     </div>
   );
 };
 
-
-export default Pedidos 
+export default Pedidos;
--- src/componentes/productos/Productos.jsx
+++ src/componentes/productos/Productos.jsx
@@ -1,19 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Productos.css';
 
 const Productos = () => {
-  const [productos, setProductos] = useState([
-    { id: 1, nombre: 'Producto 1', precio: 10, imagen: 'https://assets.adidas.com/images/w_600,f_auto,q_auto/aa53a0a800c846abb44aae8a00367e1d_9366/Zapatillas_Tensaur_Sport_Training_Cierre_de_Cordones_Blanco_GW6422_01_standard.jpg' },
-    { id: 2, nombre: 'Producto 2', precio: 20, imagen: 'https://assets.adidas.com/images/w_600,f_auto,q_auto/aa53a0a800c846abb44aae8a00367e1d_9366/Zapatillas_Tensaur_Sport_Training_Cierre_de_Cordones_Blanco_GW6422_01_standard.jpg' },
-    { id: 3, nombre: 'Producto 3', precio: 30, imagen: 'https://assets.adidas.com/images/w_600,f_auto,q_auto/aa53a0a800c846abb44aae8a00367e1d_9366/Zapatillas_Tensaur_Sport_Training_Cierre_de_Cordones_Blanco_GW6422_01_standard.jpg' },
-  ]);
-
+  const [productos, setProductos] = useState([]);
   const [indiceActual, setIndiceActual] = useState(0);
   const [nuevoProducto, setNuevoProducto] = useState({
     nombre: '',
     precio: '',
     imagen: null,
   });
+
+  useEffect(() => {
+    fetch('https://teamapi.bladimirchipana.repl.co/pruebas')
+      .then(response => response.json())
+      .then(data => setProductos(data))
+      .catch(error => console.log(error));
+  }, []);
 
   const mostrarSiguienteProducto = () => {
     setIndiceActual((indiceActual + 1) % productos.length);
@@ -71,13 +73,13 @@ const Productos = () => {
         </button>
         <div className="producto-actual">
           <img
-            src={productos[indiceActual].imagen}
-            alt={productos[indiceActual].nombre}
+            src={productos[indiceActual]?.imagen}
+            alt={productos[indiceActual]?.nombre}
             className="producto-imagen"
           />
           <div className="producto-informacion">
-            <h3>{productos[indiceActual].nombre}</h3>
-            <p>Precio: ${productos[indiceActual].precio}</p>
+            <h3>{productos[indiceActual]?.nombre}</h3>
+            <p>Precio: ${productos[indiceActual]?.precio}</p>
           </div>
         </div>
         <button className="nav-button" onClick={mostrarSiguienteProducto}>
@@ -114,74 +116,22 @@ const Productos = () => {
       <section className="container-related-products">
         <h2>Productos Relacionados</h2>
         <div className="card-list-products">
-          <div className="card">
-            <div className="card-img">
+          {productos.map((producto) => (
+            <div className="card-product" key={producto.id}>
               <img
-                src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80"
-                alt="producto-1"
+                src={producto.imagen}
+                alt={producto.nombre}
+                className="producto-imagen"
               />
-            </div>
-            <div className="info-card">
-              <div className="text-product">
-                <h3>Nike - Roshe Run</h3>
-                <p className="category">Footwear, Sneakers</p>
+              <div className="card-product-info">
+                <h3>{producto.nombre}</h3>
+                <p>Precio: ${producto.precio}</p>
               </div>
-              <div className="price">$85.00</div>
             </div>
-          </div>
-          <div className="card">
-            <div className="card-img">
-              <img
-                src="https://images.unsplash.com/photo-1587563871167-1ee9c731aefb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1131&q=80"
-                alt="producto-2"
-              />
-            </div>
-            <div className="info-card">
-              <div className="text-product">
-                <h3>Common Projects Achilles</h3>
-                <p className="category">Footwear, Sneakers</p>
-              </div>
-              <div className="price">$255.00</div>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-img">
-              <img
-                src="https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                alt="producto-3"
-              />
-            </div>
-            <div className="info-card">
-              <div className="text-product">
-                <h3>Adidas - Boston Super OG</h3>
-                <p className="category">Footwear, Sneakers</p>
-              </div>
-              <div className="price">$105.00</div>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-img">
-              <img
-                src="https://images.unsplash.com/photo-1617606002779-51d866bdd1d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                alt="producto-4"
-              />
-            </div>
-            <div className="info-card">
-              <div className="text-product">
-                <h3>Common Projects Achilles</h3>
-                <p className="category">Footwear, Sneakers</p>
-              </div>
-              <div className="price">$250.00</div>
-            </div>
-          </div>
+          ))}
         </div>
       </section>
-      <footer>
-  <p>TeamFy</p>
-</footer>
-
     </div>
-    
   );
 };
 
